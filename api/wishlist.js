d87c31304b98917cd7cc83d801e3bb47d3edc5bc
Canis Ouambo <ouambocanis@gmail.com>
2025-02-02 03:05:53 +0000

    Add Swimming Goggles in the wishlist

--- api/wishlist.js
+++ api/wishlist.js
@@ -8,8 +8,8 @@ module.exports = (req, res) => {
         { id: 5, item: 'Bose SoundLink Flex Bluetooth® Speaker', price: '£119.95', url: 'https://www.bose.co.uk/en_gb/products/speakers/portable_speakers/soundlink-flex.html#v=soundlink_flex_black', claimed: false },
         { id: 6, item: '4K HD Spy Camera WiFi Hidden Camera Clock', price: '£79.99', url: 'https://www.amazon.co.uk/gp/product/B0DN1GKXJG/ref=ox_sc_act_title_1?smid=A758J3OG2RWGJ&psc=1', claimed: false },
         { id: 7, item: 'Stanley Classic Legendary Thermal Flask 1.9L', price: '£57.50', url: 'https://www.amazon.co.uk/Stanley-Classic-Legendary-Vacuum-Bottle/dp/B001EN21BS', claimed: false },
-        { id: 8, item: 'Igluu Meal Prep Glass Containers 10 Pack [Rectangle]', price: '£39.95', url: 'https://www.amazon.co.uk/gp/product/B07XJ68DCP/ref=ewc_pr_img_6?smid=A32PQL907CZRIA&th=1', claimed: false }
-        
+        { id: 8, item: 'Igluu Meal Prep Glass Containers 10 Pack [Rectangle]', price: '£39.95', url: 'https://www.amazon.co.uk/gp/product/B07XJ68DCP/ref=ewc_pr_img_6?smid=A32PQL907CZRIA&th=1', claimed: false },
+        { id: 9, item: 'Swimming Goggles', price: '£17.99', url: 'https://amzn.eu/d/bWJ6JbW', claimed: false}
     ];
 
     if (req.method === 'GET') {
