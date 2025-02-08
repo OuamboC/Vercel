f586be8ca6db9c828c917f5dc75421aaf8b5fdf1
Canis Ouambo <118950061+OuamboC@users.noreply.github.com>
2025-02-08 17:57:03 +0000

    feat(wishlist): replace out-of-stock Bose SoundLink Flex with JBL Flip 6

--- api/wishlist.js
+++ api/wishlist.js
@@ -4,7 +4,7 @@ let wishlist = [
     { id: 3, item: 'Aegis Fortress XTS Encryption [1 TB]', price: '£224.96', url:'https://apricorn.com/aegis-fortress-l3?gad_source=1&gbraid=0AAAAAD_h3UtN66pq3tm_zufReYpOxlPpj&gclid=CjwKCAiAqfe8BhBwEiwAsne6gXSO0io-1exk10rvwgvX3zLw6wjj075Iw7HBWE2tGLyXOfvvMaE_bBoCFD0QAvD_BwE', claimed:false },
     { id: 4, item: 'Xbox Elite Series 2 Wireless Controller', price: '£149', url: 'https://www.xbox.com/en-GB/accessories/controllers/elite-wireless-controller-series-2', claimed: false },
     { id: 5, item: 'Dell 330W 7.4mm GaN Slim AC Adapter', price: '£146.08', url: 'https://www.dell.com/en-uk/shop/dell-330w-74mm-gan-slim-ac-adapter/apd/450-bcbx/pc-accessories', claimed: false },
-    { id: 6, item: 'Bose SoundLink Flex Bluetooth® Speaker', price: '£119.95', url: 'https://www.bose.co.uk/en_gb/products/speakers/portable_speakers/soundlink-flex.html#v=soundlink_flex_black', claimed: false },
+    { id: 6, item: 'JBL Flip 6', price: '£129.99', url: 'https://uk.jbl.com/FLIP-6-.html?cgid=speakers', claimed: false },
     { id: 7, item: '4K HD Spy Camera WiFi Hidden Camera Clock', price: '£79.99', url: 'https://www.amazon.co.uk/gp/product/B0DN1GKXJG/ref=ox_sc_act_title_1?smid=A758J3OG2RWGJ&psc=1', claimed: false },
     { id: 8, item: 'Stanley Classic Legendary Thermal Flask 1.9L', price: '£57.50', url: 'https://www.amazon.co.uk/Stanley-Classic-Legendary-Vacuum-Bottle/dp/B001EN21BS', claimed: false },
     { id: 9, item: 'Testing', price: '£1', url: 'https://www.tissotwatches.com/en-gb/T1294103626100.html', claimed: false }
